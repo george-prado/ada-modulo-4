355d7f0d4d028dba7bac7c2160aa7c69f92aed3e
george-prado <gmeloprado@gmail.com>
2024-04-11 19:11:32 -0300

    fix: fixed theme name description on nav

--- theme.js
+++ theme.js
@@ -1,22 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const themeBtn = document.querySelector('#btnTheme');
+    const themeName = document.querySelector('#themeName');
     const body = document.body;
 
     themeBtn.addEventListener('change', function () {
         if (themeBtn.checked) {
             body.classList.remove('light-theme');
             body.classList.add('dark-theme');
+            themeName.innerText = 'Tema Escuro';
         } else {
             body.classList.remove('dark-theme');
             body.classList.add('light-theme');
+            themeName.innerText = 'Tema Claro';
         }
     });
-
-    if (themeBtn.checked) {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-    } else {
-        body.classList.remove('dark-theme');
-        body.classList.add('light-theme');
-    }
 });
